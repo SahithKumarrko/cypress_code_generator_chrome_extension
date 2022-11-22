dababb13f499d9ad14f05ff8a16564b080af1d3a
SahithKumarrko <sahithkumarrko@gmail.com>
2022-11-22 14:59:21 +0530

    changed

--- popups/popup.js
+++ popups/popup.js
@@ -9,66 +9,65 @@ function sendMessageToBackground(request, callback = (response) => { }) {
     chrome.runtime.sendMessage(request, callback);
 }
 
-function start_stop() {
-    console.log("querying");
-    var action_btn = document.getElementById("start_stop");
+function start_recording() {
+    var start_btn = document.getElementById("start");
+    var stop_btn = document.getElementById("stop");
     var pause_btn = document.getElementById("pause");
-    var refresh_btn = document.getElementById("refresh");
-    if (action_btn.getAttribute("value") == "start") {
-        console.log("Starting");
-        action_btn.classList = "fa fa-stop stop action-btn";
-        pause_btn.style.display = "inline-block";
-        refresh_btn.style.display = "inline-block";
-        action_btn.setAttribute("value", "stop");
-        action_btn.setAttribute("title", "Stop Recording");
-        pause_btn.setAttribute("title", "Pause");
-        chrome.tabs.query({ active: true, currentWindow: true }, function (d) {
-            // if (current_tab == d[0].id) {
+    stop_btn.style.display = "inline-block";
+    pause_btn.style.display = "inline-block";
+    start_btn.style.display = "none";
+    pause_btn.setAttribute("title", "Pause");
+    chrome.tabs.query({ active: true, currentWindow: true }, function (d) {
+        // if (current_tab == d[0].id) {
 
-            // } else {
-            current_tab = d[0].id;
-            var url = d[0].url;
-            console.log("Sending message")
-            data.activeTab = current_tab;
-            data.url = url;
-            data.state = "start";
-            sendMessage(current_tab, data, (response) => {
-                console.log("Received start response", response);
-
-            });
-            chrome.storage.session.set(data);
-            // sendMessageToBackground({ from: "popup", to: "background", tabId: current_tab, url: url }, (response) => { console.log("Received background response :: ", response) });
-
-
-            // }
-        });
-    } else {
-        console.log("Stoping");
-        pause_btn.style.display = "none";
-        action_btn.setAttribute("title", "Start Recording");
-        pause_btn.setAttribute("title", "Pause");
-        action_btn.classList = "fa fa-play start action-btn";
-        action_btn.setAttribute("value", "start");
-        data.activeTab = -1;
-        data.url = "";
-        data.state = "stop";
+        // } else {
+        current_tab = d[0].id;
+        var url = d[0].url;
+        console.log("Sending message")
+        data.activeTab = current_tab;
+        data.url = url;
+        data.state = "start";
         sendMessage(current_tab, data, (response) => {
-            console.log("Received stop response", response);
+            console.log("Received start response", response);
 
         });
         chrome.storage.session.set(data);
-    }
+        // sendMessageToBackground({ from: "popup", to: "background", tabId: current_tab, url: url }, (response) => { console.log("Received background response :: ", response) });
 
+
+        // }
+    });
 }
 
-function pause() {
+function stop_recording() {
+    var start_btn = document.getElementById("start");
+    var stop_btn = document.getElementById("stop");
+    var pause_btn = document.getElementById("pause");
+    console.log("Stoping");
+    start_btn.style.display = "inline-block";
+    start_btn.setAttribute("title", "Start Recording");
+    start_btn.classList = "fa fa-play action-btn";
+    pause_btn.style.display = "none";
+    stop_btn.style.display = "none";
+    data.activeTab = -1;
+    data.url = "";
+    data.state = "stop";
+    sendMessage(current_tab, data, (response) => {
+        console.log("Received stop response", response);
+
+    });
+    chrome.storage.session.set(data);
+}
+
+function pause_recording() {
+    var start_btn = document.getElementById("start");
+    var stop_btn = document.getElementById("stop");
     var pause_btn = document.getElementById("pause");
     pause_btn.style.display = "none";
-    pause_btn.setAttribute("title", "Pause");
-    var action_btn = document.getElementById("start_stop");
-    action_btn.setAttribute("title", "Resume Recording");
-    action_btn.classList = "fa fa-play start-pause action-btn";
-    action_btn.setAttribute("value", "start");
+    start_btn.setAttribute("title", "Resume Recording");
+    start_btn.classList = "fa fa-play start-pause action-btn";
+    start_btn.style.display = "inline-block";
+    stop_btn.style.display = "inline-block";
     data.state = "pause";
     sendMessage(current_tab, data, (response) => {
         console.log("Received pause response", response);
@@ -77,46 +76,43 @@ function pause() {
 
 }
 
-function refresh() {
+function refresh_recording() {
     document.getElementById("refresh").classList = "fa fa-refresh fa-spin action-btn";
     //TODO :: Refreshing
     setTimeout(() => document.getElementById("refresh").classList = "fa fa-refresh action-btn", 3000);
 }
 
 function reveal() {
-    var action_btn = document.getElementById("start_stop");
-    var refresh_btn = document.getElementById("refresh");
+    var start_btn = document.getElementById("start");
+    var stop_btn = document.getElementById("stop");
     var pause_btn = document.getElementById("pause");
     if (data.state == "start") {
         console.log("Already Started");
-        action_btn.classList = "fa fa-stop stop action-btn";
-        action_btn.setAttribute("title", "Stop Recording");
+        start_btn.style.display = "none";
+        stop_btn.style.display = "inline-block";
         pause_btn.style.display = "inline-block";
-        refresh_btn.style.display = "inline-block";
-        action_btn.setAttribute("value", "stop");
 
     } else if (data.state == "pause") {
         console.log("Paused");
         pause_btn.style.display = "none";
-        action_btn.classList = "fa fa-play start-pause action-btn";
-        action_btn.setAttribute("title", "Resume Recording");
-        action_btn.setAttribute("value", "start");
-        refresh_btn.style.display = "inline-block";
+        start_btn.classList = "fa fa-play start-pause action-btn";
+        start_btn.setAttribute("title", "Resume Recording");
+        stop_btn.style.display = "inline-block";
     }
     else {
         console.log("Stopped");
+        start_btn.style.display = "inline-block";
         pause_btn.style.display = "none";
-        action_btn.classList = "fa fa-play start action-btn";
-        action_btn.setAttribute("title", "Start Recording");
-        action_btn.setAttribute("value", "start");
+        stop_btn.style.display = "none";
     }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    document.getElementById("start_stop").addEventListener("click", start_stop);
-    document.getElementById("pause").addEventListener("click", pause);
-    document.getElementById("refresh").addEventListener("click", refresh);
+    document.getElementById("start").addEventListener("click", start_recording);
+    document.getElementById("stop").addEventListener("click", stop_recording);
+    document.getElementById("pause").addEventListener("click", pause_recording);
+    document.getElementById("refresh").addEventListener("click", refresh_recording);
     chrome.storage.session.get(Object.keys(data)).then((v) => {
         console.log("current tab ", v);
         current_tab = v.activeTab;
